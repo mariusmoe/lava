e6878bd5ad47b9362f95b50ff5bb9f86b2e553cd
Unknown <mariusomoe@gmail.com>
2018-03-22 17:16:50 +0100

    One test

--- FloorIsLava/core/src/test/java/com/lava/game/states/PlayStateTest.java
+++ FloorIsLava/core/src/test/java/com/lava/game/states/PlayStateTest.java
@@ -1,13 +1,22 @@
 package com.lava.game.states;
 
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by moe on 22.03.18.
  */
+
 public class PlayStateTest {
     @org.junit.Test
     public void intToByteArray() throws Exception {
+        byte[] aa = {(byte) 0, (byte) 0, (byte) 0, (byte) 1};
+        byte[] bb = {(byte) 0, (byte) 0, (byte) 0, (byte) 2};
+        assertEquals(Arrays.toString(PlayState.intToByteArray(1)), Arrays.toString(aa));
+        assertNotEquals(Arrays.toString(PlayState.intToByteArray(1)), Arrays.toString(bb));
     }
 
     @org.junit.Test
