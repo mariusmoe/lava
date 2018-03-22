59b5d54dd5a74290c0f4e05ba0a0c22c8e0d7415
Unknown <mariusomoe@gmail.com>
2018-03-22 16:42:22 +0100

    Remove broken tests

---
+++ FloorIsLava/core/src/test/java/com/lava/game/states/PlayStateTest.java
@@ -0,0 +1,17 @@
+package com.lava.game.states;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by moe on 22.03.18.
+ */
+public class PlayStateTest {
+    @org.junit.Test
+    public void intToByteArray() throws Exception {
+    }
+
+    @org.junit.Test
+    public void byteArrayToInt() throws Exception {
+    }
+
+}
